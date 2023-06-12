4d9e1450eca29a8a864620edeb7b4216229247af
mykysyk <mykysyk@MacBookPro-2.local>
2023-06-12 10:30:59 +0900

    gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-context-sample' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/nextjs-context-sample' : '',
+  
+}
 
 module.exports = nextConfig
