02e830f8347de96444afaa7b7b65d95f99a59632
mykysyk <mykysyk@MacBookPro-2.local>
2023-06-12 11:34:35 +0900

    fix gh-pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-context-sample' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/nextjs-context-sample' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-context-sample' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/nextjs-context-sample' : '',
+  assetPrefix: isProduction ?  '/nextjs-context-sample' : '',
+  basePath: isProduction ?  '/nextjs-context-sample' : '',
   
 }
 
